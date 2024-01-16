34a370263cfcb1d92e71cfbd01d41da8ff25f853
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2024-01-16 12:24:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st  
+import numpy as np 
+import pandas as pd
+import altair as alt
+
+st.set_page_config(page_title="PiJunior")
+
+st.title("PiJunior")
+st.header("Precificação de Projetos PiJunior")
+st.subheader("Base")
+st.write("nome do projeto") 
+st.markdown("Markdown") 
+st.caption("caption")
+st.button("botão")
+st.text("text")
+st.latex("latex")
+st.code("code")
